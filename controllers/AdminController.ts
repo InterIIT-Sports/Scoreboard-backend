653577072e54ee20d533b8ec18e151ce52369cd1
Karan Gandhi <karangandhi.programming@gmail.com>
2023-11-15 16:57:02 +0530

    documented the admin routes

--- controllers/AdminController.ts
+++ controllers/AdminController.ts
@@ -6,17 +6,28 @@ import CreateUserWithUsernameAndPasswordRequest from "../requests/CreateUserWith
 @Route("admin")
 @Tags("Admin")
 export class AdminController {
+  /**
+   * Retrieves all users from the database.
+   */
   @Get("users")
   public async getUsers() {
     return await getAllUsers();
   }
 
+  /**
+   * Deletes a user with the given username.
+   * @param {DeleteUserRequest} request - The request object containing the username of the user to be deleted.
+   */
   @Delete("user")
   @Response(204)
   public async deleteUser(@Body() { username }: DeleteUserRequest) {
     await deleteUser(username);
   }
 
+  /**
+   * Creates a new user with the given name, username, password, and role.
+   * @param {CreateUserWithUsernameAndPasswordRequest} request - The request object containing name, username, password, and role.
+   */
   @Post("createUserWithUsernameAndPassword")
   @Response(403, "Username already exists")
   public async createUserWithUsernameAndPassword(
