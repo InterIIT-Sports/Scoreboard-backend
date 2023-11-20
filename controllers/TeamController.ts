844693a2c4f70c95582ea54167b68fa5c73050d9
Karan Gandhi <karangandhi.programming@gmail.com>
2023-11-20 12:52:13 +0530

    documented a teams routes

--- controllers/TeamController.ts
+++ controllers/TeamController.ts
@@ -1,20 +1,32 @@
-import { Body, Delete, Get, Path, Post, Route, Tags } from "tsoa";
+import { Body, Delete, Get, Header, Middlewares, Path, Post, Route, Tags } from "tsoa";
 import { addTeam, deleteTeam, getAllTeams } from "../utils/TeamUtils";
 import CreateTeamRequest from "../requests/CreateTeamRequest";
 
 @Route("admin/teams")
 @Tags("Teams")
 export class TeamControllers {
+  /**
+   * Retrieves all teams.
+   */
   @Get("")
   public async getAllTeams() {
     return await getAllTeams();
   }
 
+  /**
+   * Adds a team.
+   *
+   * @param name - The name of the team.
+   */
   @Post("")
   public async addTeam(@Body() { name }: CreateTeamRequest) {
     await addTeam(name);
   }
 
+  /**
+   * Deletes a team.
+   * @param id - The ID of the team to be deleted.
+   */
   @Delete("/:id")
   public async deleteTeam(@Path("id") id: string) {
     await deleteTeam(id);
--- routes/TeamRoutes.ts
+++ routes/TeamRoutes.ts
@@ -5,14 +5,14 @@ const router = express.Router();
 
 router.get("/", async (req, res) => {
   res.json(await new TeamControllers().getAllTeams());
-}); // get all teams
+});
 
 router.post("/", async (req, res) => {
   res.json(await new TeamControllers().addTeam(req.body));
-}); // create a team
+});
 
 router.delete("/:id", async (req, res) => {
   res.json(await new TeamControllers().deleteTeam(req.params.id));
-}); // delete a team
+});
 
 export default router;
