fa03327e24063cb3d1c6863b78ab29765eb4b177
Karan Gandhi <karangandhi.programming@gmail.com>
2023-11-25 23:44:04 +0530

    fixed the cors error for connecting to the socket server

--- utils/ServerUtils.ts
+++ utils/ServerUtils.ts
@@ -3,13 +3,14 @@ import { connectToDatabase } from "./DatabaseUtils";
 import { Server } from "socket.io";
 import { addSocketEvents } from "./SocketUtils";
 import { SocketServer } from "../types/SocketServer";
+import { CorsConfig } from "../config/CorsConfig";
 
 const PORT = process.env.PORT || 5000;
 
 export const createAndStartServer = async (app: Express) => {
   await connectToDatabase();
   const server = app.listen(PORT, () => console.log("[S] Server started at port: " + PORT));
-  const _io = new Server(server);
+  const _io = new Server(server, { cors: CorsConfig });
   SocketServer.io = _io;
   addSocketEvents();
 };
